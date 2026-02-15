942566a69f802919f3daa7e2e1eee6aad6ea7239
MoulinLouis <MoulinLouis@users.noreply.github.com>
2026-02-15 01:47:21 +0100

    Fix HelloCommand: use PlayerRef for username lookup

--- hyvexa-core/src/main/java/io/hyvexa/common/command/HelloCommand.java
+++ hyvexa-core/src/main/java/io/hyvexa/common/command/HelloCommand.java
@@ -1,11 +1,15 @@
 package io.hyvexa.common.command;
 
+import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.Message;
 import com.hypixel.hytale.server.core.command.system.CommandContext;
 import com.hypixel.hytale.server.core.command.system.CommandSender;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractAsyncCommand;
 import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 import javax.annotation.Nonnull;
 import java.util.concurrent.CompletableFuture;
@@ -22,7 +26,12 @@ public class HelloCommand extends AbstractAsyncCommand {
     protected CompletableFuture<Void> executeAsync(CommandContext ctx) {
         CommandSender sender = ctx.sender();
         if (sender instanceof Player player) {
-            player.sendMessage(Message.raw("Hi, " + player.getUsername() + "!"));
+            Ref<EntityStore> ref = player.getReference();
+            if (ref == null || !ref.isValid()) return CompletableFuture.completedFuture(null);
+            Store<EntityStore> store = ref.getStore();
+            PlayerRef playerRef = store.getComponent(ref, PlayerRef.getComponentType());
+            String name = playerRef != null ? playerRef.getUsername() : "player";
+            player.sendMessage(Message.raw("Hi, " + name + "!"));
         } else {
             ctx.sendMessage(Message.raw("Hi!"));
         }
