f52eff5cb57fee9f700c98f77fd79dd9ab5714a2
Onakar <33369818+GuillaumeRey07@users.noreply.github.com>
2026-02-20 22:03:57 +0100

    feat(runorfall): HUD

---
+++ hyvexa-runorfall/src/main/java/io/hyvexa/runorfall/hud/RunOrFallHud.java
@@ -0,0 +1,42 @@
+package io.hyvexa.runorfall.hud;
+
+import com.hypixel.hytale.server.core.entity.entities.player.hud.CustomUIHud;
+import com.hypixel.hytale.server.core.ui.builder.UICommandBuilder;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.Universe;
+
+public class RunOrFallHud extends CustomUIHud {
+
+    private int lastPlayerCount = -1;
+    private long lastGems = -1;
+
+    public RunOrFallHud(PlayerRef playerRef) {
+        super(playerRef);
+    }
+
+    @Override
+    protected void build(UICommandBuilder commandBuilder) {
+        commandBuilder.append("Pages/RunOrFall_RunHud.ui");
+    }
+
+    public void updateGems(long gems) {
+        if (gems == lastGems) {
+            return;
+        }
+        lastGems = gems;
+        UICommandBuilder commandBuilder = new UICommandBuilder();
+        commandBuilder.set("#PlayerGemsValue.Text", String.valueOf(gems));
+        update(false, commandBuilder);
+    }
+
+    public void updatePlayerCount() {
+        int count = Universe.get().getPlayers().size();
+        if (count == lastPlayerCount) {
+            return;
+        }
+        lastPlayerCount = count;
+        UICommandBuilder commandBuilder = new UICommandBuilder();
+        commandBuilder.set("#PlayerCountText.Text", String.format("%,d", count));
+        update(false, commandBuilder);
+    }
+}
